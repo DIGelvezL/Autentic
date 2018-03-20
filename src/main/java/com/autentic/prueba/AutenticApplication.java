21949dd677dd92b8c199fa5443296cb6b3c9ac80
INTERGRUPO\Dgelvez <dgelvez@intergrupo.com>
2018-03-20 14:30:58 -0500

    first commit

---
+++ src/main/java/com/autentic/prueba/AutenticApplication.java
@@ -0,0 +1,12 @@
+package com.autentic.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticApplication.class, args);
+	}
+}
