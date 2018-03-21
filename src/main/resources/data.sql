5a93b958da253e4994de6acbc666a210e564fe65
INTERGRUPO\Dgelvez <dgelvez@intergrupo.com>
2018-03-21 09:03:58 -0500

    no message

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into productos (id, nombre, valor) values (1, 'TV LG 55"', '1800000');
+insert into productos (id, nombre, valor) values (2, 'Nevera Haceb', '1200000');
+insert into productos (id, nombre, valor) values (3, 'Computador', '5900000');
+insert into productos (id, nombre, valor) values (4, 'Lavadora', '1100000');
+insert into productos (id, nombre, valor) values (5, 'Celular Samsung S7 Edge', '1500000');
+insert into productos (id, nombre, valor) values (6, 'Kaeasaki Ninja 300', '19000000');
+insert into productos (id, nombre, valor) values (7, 'Play Station 4', '1450000');
