33c5bfd9cfc5c13fc5d65a197c5fbb56a9d38bc7
dgelvez <dgelvez@intergrupo.com>
2018-03-21 20:46:16 -0500

    no message

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,5 @@ insert into productos (id, nombre, valor) values (2, 'Nevera Haceb', '1200000');
 insert into productos (id, nombre, valor) values (3, 'Computador', '5900000');
 insert into productos (id, nombre, valor) values (4, 'Lavadora', '1100000');
 insert into productos (id, nombre, valor) values (5, 'Celular Samsung S7 Edge', '1500000');
-insert into productos (id, nombre, valor) values (6, 'Kaeasaki Ninja 300', '19000000');
+insert into productos (id, nombre, valor) values (6, 'Kawasaki Ninja 300', '19000000');
 insert into productos (id, nombre, valor) values (7, 'Play Station 4', '1450000');
